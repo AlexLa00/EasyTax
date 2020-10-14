ea2f4aa786b7111d755032b1bc03f174f850ede8
CookieMonster95 <71412382+CookieMonster95@users.noreply.github.com>
2020-10-13 21:36:59 -0400

    implemented login activity and connected welcome page to login

--- app/src/main/java/app/cyberzen/easytax/WelcomeActivity.java
+++ app/src/main/java/app/cyberzen/easytax/WelcomeActivity.java
@@ -2,13 +2,28 @@ package app.cyberzen.easytax;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import app.cyberzen.easytax.ui.login.LoginActivity;
 
 public class WelcomeActivity extends AppCompatActivity {
+    private Button loginBtn;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.welcome);
+        loginBtn =(Button) findViewById(R.id.LoginBtn);
+        loginBtn.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v){
+                Intent login = new Intent(getApplicationContext(), LoginActivity.class);
+                startActivity(login);
+            }
+        });
     }
-}
+
+    }
