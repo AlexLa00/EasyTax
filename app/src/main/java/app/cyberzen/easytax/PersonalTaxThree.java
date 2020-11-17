ab8e436f799fc20499f606d6651bdf29d124b64d
GireeshSuperSunny3101 <71040997+GireeshSuperSunny3101@users.noreply.github.com>
2020-11-16 22:29:40 -0500

    Started getting user inputs

---
+++ app/src/main/java/app/cyberzen/easytax/PersonalTaxThree.java
@@ -0,0 +1,13 @@
+package app.cyberzen.easytax;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class PersonalTaxThree extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_personal_tax_three);
+    }
+}
