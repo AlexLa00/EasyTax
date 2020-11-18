4fa67141e7ad15b5e87b0d9d65a3f91dbc2a9964
GireeshSuperSunny3101 <71040997+GireeshSuperSunny3101@users.noreply.github.com>
2020-11-17 20:24:12 -0500

    sorted out the buttons of PersonalTaxOne to PersonalTaxTwo to PersonalTaxThree

--- app/src/main/java/app/cyberzen/easytax/HomeScreen.java
+++ app/src/main/java/app/cyberzen/easytax/HomeScreen.java
@@ -2,13 +2,31 @@ package app.cyberzen.easytax;
 
         import androidx.appcompat.app.AppCompatActivity;
 
+        import android.content.Intent;
         import android.os.Bundle;
+        import android.view.View;
+        import android.widget.Button;
 
 public class HomeScreen extends AppCompatActivity {
+    private Button button;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home_screen);
+
+        button = (Button) findViewById(R.id.PersonalTax);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                openPersonalTaxOne();
+            }
+        });
+
     }
+
+        public void openPersonalTaxOne(){
+            Intent intent = new  Intent(this, PersonalTaxOne.class);
+            startActivity(intent);
+        }
 }
