3989e96361d87bf111cdd0907fb82b614ea9ccc4
yogeshwarmahadeo <yogeshwar.mahadeo@humber.ca>
2020-12-11 02:46:21 -0500

    Update with calculations

--- EasyTax/src/main/java/app/cyberzen/easytax/BusinessTaxForm.java
+++ EasyTax/src/main/java/app/cyberzen/easytax/BusinessTaxForm.java
@@ -16,6 +16,7 @@ public class BusinessTaxForm extends AppCompatActivity {
     Button submit;
     EditText yearlyincome;
     Button saved;
+    EditText previousIncome;
 
 
 
@@ -35,57 +36,59 @@ public class BusinessTaxForm extends AppCompatActivity {
             }
         });
 
-                    submit.setOnClickListener(new View.OnClickListener() {
-                        @Override
-                        public void onClick(View view) {
-                            onButtonCalculateClick(view);
-                            String income = yearlyincome.getText().toString();
+        submit.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                onButtonCalculateClick(view);
+                String income = yearlyincome.getText().toString();
 
-                            Intent intent = new Intent(BusinessTaxForm.this, TotalClaimAmount.class);
-                            intent.putExtra("key", income);
+                Intent intent = new Intent(BusinessTaxForm.this, TotalClaimAmount.class);
+                intent.putExtra("key", income);
 
-                            startActivity(intent);
-                        }
-                    });
-                }
-
-                public void onButtonCalculateClick(View v) {
-                    double income, tax, total;
-                    int status = 0;
-                    tax = 0;
-
-                    EditText yearlyIncome = (EditText) findViewById(R.id.yearlyIncome);
-
-                    TextView t1 = (TextView) findViewById(R.id.taxesOwed);
-                    income = Integer.parseInt(yearlyIncome.getText().toString());
-
-
-
-                    //Candian income tax reference
-                    //https://www.canada.ca/en/revenue-agency/services/tax/individuals/frequently-asked-questions-individuals/canadian-income-tax-rates-individuals-current-previous-years.html
-
-                    if (status == 0) {
-                        if (income <= 11950)
-                            tax = income * 0.10;
-                        else if (income <= 45500)
-                            tax = 11950 * 0.10 + (income - 11950) * 0.15;
-                        else if (income <= 117450)
-                            tax = 11950 * 0.10 + (45500 - 11950) * 0.15 + (income - 45500) * 0.25;
-                        else if (income <= 190200)
-                            tax = 11950 * 0.10 + (45500 - 11950) * 0.15 + (117450 - 45500) * 0.25 +
-                                    (income - 117450) * 0.28;
-                        else if (income <= 372950)
-                            tax = 11950 * 0.10 + (45500 - 11950) * 0.15 + (117450 - 45500) * 0.25 +
-                                    (190200 - 117450) * 0.28 + (income - 190200) * 0.33;
-                        else
-                            tax = 11950 * 0.10 + (45500 - 11950) * 0.15 +
-                                    (117451 - 45500) * 0.25 + (190200 - 117450) * 0.28 +
-                                    (372950 - 190200) * 0.33 + (income - 372950) * 0.35;
-                    } else {
-                    }
-                    total = ((tax * 100) / 100.0);
-                    yearlyIncome.setText("$" + Double.toString(total));
-
-                }
-
+                startActivity(intent);
             }
+        });
+    }
+
+    public void onButtonCalculateClick(View v) {
+        double income, tax, total;
+        int status = 0;
+        int pIncome;
+        tax = 0;
+
+        EditText yearlyIncome = (EditText) findViewById(R.id.yearlyIncome);
+        previousIncome = (EditText) findViewById(R.id.paidIncome3);
+        pIncome = Integer.parseInt(previousIncome.getText().toString());
+        TextView t1 = (TextView) findViewById(R.id.taxesOwed);
+        income = Integer.parseInt(yearlyIncome.getText().toString());
+
+
+
+        //Candian income tax reference
+        //https://www.canada.ca/en/revenue-agency/services/tax/individuals/frequently-asked-questions-individuals/canadian-income-tax-rates-individuals-current-previous-years.html
+
+        if (status == 0) {
+            if (income <= 11950)
+                tax = (income  * 0.10)- pIncome;
+            else if (income <= 45500)
+                tax = (11950 * 0.10 + (income - 11950) * 0.15) - pIncome;
+            else if (income <= 117450)
+                tax = (11950 * 0.10 + (45500 - 11950) * 0.15 + (income - 45500) * 0.25)- pIncome ;
+            else if (income <= 190200)
+                tax = (11950 * 0.10 + (45500 - 11950) * 0.15 + (117450 - 45500) * 0.25 +
+                        (income - 117450) * 0.28) - pIncome;
+            else if (income <= 372950)
+                tax = (11950 * 0.10 + (45500 - 11950) * 0.15 + (117450 - 45500) * 0.25  +
+                        (190200 - 117450) * 0.28 + (income - 190200) * 0.33) -pIncome;
+            else
+                tax = (11950 * 0.10 + (45500 - 11950) * 0.15 +
+                        (117451 - 45500) * 0.25 + (190200 - 117450) * 0.28 +
+                        (372950 - 190200) * 0.33 + (income - 372950) * 0.35) - pIncome ;
+        } else {
+        }
+        total = ((tax * 100) / 100.0);
+        yearlyIncome.setText("$" + Double.toString(total));
+
+    }
+
+}
--- EasyTax/src/main/java/app/cyberzen/easytax/FamilyTaxForm.java
+++ EasyTax/src/main/java/app/cyberzen/easytax/FamilyTaxForm.java
@@ -14,6 +14,7 @@ import com.google.android.material.snackbar.Snackbar;
 public class FamilyTaxForm extends AppCompatActivity {
     Button submit;
     EditText yearlyincome;
+    EditText previousIncome;
     Button saved;
 
 
@@ -48,11 +49,14 @@ public class FamilyTaxForm extends AppCompatActivity {
     public void onButtonCalculateClick(View v) {
         double income, tax, total;
         int status = 0;
+        int pIncome;
         tax = 0;
 
         EditText yearlyIncome = (EditText)findViewById(R.id.yearlyIncome);
         TextView t1 = (TextView)findViewById(R.id.taxesOwed);
         income = Integer.parseInt(yearlyIncome.getText().toString());
+        previousIncome = (EditText) findViewById(R.id.paidIncome1);
+        pIncome = Integer.parseInt(previousIncome.getText().toString());
 
         //Canadian income tax reference
         //https://www.canada.ca/en/revenue-agency/services/tax/individuals/frequently-asked-questions-individuals/canadian-income-tax-rates-individuals-current-previous-years.html
@@ -60,21 +64,21 @@ public class FamilyTaxForm extends AppCompatActivity {
         if (status == 0)
         {
             if (income <= 16700)
-                tax = income * 0.505;
+                tax = ((income * 0.505) - pIncome);
             else if (income <= 67900)
-                tax = 16700 * 0.10 + (income - 16700) * 0.15;
+                tax = (16700 * 0.10 + (income - 16700) * 0.15) - pIncome;
             else if (income <= 137050)
-                tax = 16700 * 0.10 + (67900 - 16700) * 0.15 + (income - 67900) * 0.25;
+                tax = (16700 * 0.10 + (67900 - 16700) * 0.15 + (income - 67900) * 0.25) - pIncome;
             else if (income <= 208850)
-                tax = 16700 * 0.10 + (67900 - 16700) * 0.15 + (137050 - 67900) * 0.25 +
-                        (income - 137050) * 0.28;
+                tax = (16700 * 0.10 + (67900 - 16700) * 0.15 + (137050 - 67900) * 0.25 +
+                        (income - 137050) * 0.28) - pIncome;
             else if (income <= 372950)
-                tax = 16700 * 0.10 + (67900 - 16700) * 0.15 + (137050 - 67900) * 0.25 +
-                        (208850 - 137050) * 0.28 + (income - 208850) * 0.33;
+                tax = (16700 * 0.10 + (67900 - 16700) * 0.15 + (137050 - 67900) * 0.25 +
+                        (208850 - 137050) * 0.28 + (income - 208850) * 0.33) - pIncome;
             else
-                tax = 16700 * 0.10 + (67900 - 16700) * 0.15 +
+                tax = (16700 * 0.10 + (67900 - 16700) * 0.15 +
                         (137050 - 67900) * 0.25 + (208850 - 137050) * 0.28 +
-                        (372950 - 208850) * 0.33 + (income - 372950) * 0.35;
+                        (372950 - 208850) * 0.33 + (income - 372950) * 0.35) - pIncome;
         }
         else{}
         total = ((tax * 100) / 100.0);
